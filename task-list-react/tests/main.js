07ad2179cdda7fb53f99c0c739c753fc67adb307
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-12-21 12:07:36 +0100

    uploading meteor project

--- task-list-react/tests/main.js
+++ task-list-react/tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("task-list-react", function () {
+describe("simple-todos-react", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "task-list-react");
+    assert.strictEqual(name, "simple-todos-react");
   });
 
   if (Meteor.isClient) {
