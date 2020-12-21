5e5f445233d8e610243b4ab6bb9aff1e6f4d57bf
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-12-21 12:02:44 +0100

    first upload

---
+++ imports/ui/Task.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Task = ({ task, onCheckBoxClick, onDeleteClick }) => {
+    return (
+        <li className='task'>
+            <input
+                type='checkbox'
+                checked={!!task.isChecked}
+                onClick={() => onCheckBoxClick(task)}
+                readOnly
+            />
+            <span>{task.text}</span>
+            <button onClick={ () => onDeleteClick(task)}>&times;</button>
+        </li>
+
+    )
+}
